ce13bbd8b0e8c79d6db6d0fbb99dc56c37c9b266
Sugita <moriaki.sugita.1730@gmail.com>
2026-01-02 22:27:04 +0900

    tailwindの設定を追加

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     host: true, // Dockerなどの外部からの接続を許可
     port: 5173,
